d68d49e94e48e92277ac5b929dc35bb534138852
bjoexxxx <bjoe6702@stud.kea.dk>
2022-04-19 09:14:48 +0200

    First commit with gitIgnore

---
+++ src/com/company/Pizza.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Pizza {
+  private String name;
+  private ArrayList<String> ingredients;
+  public Pizza(String name, ArrayList<String> ingredients){
+    this.name = name;
+    this.ingredients = ingredients;
+  }
+}
